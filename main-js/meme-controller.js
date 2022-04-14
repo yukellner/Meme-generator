5bb56df69f0d015ce1f43e104a297c0d45c1f546
Yuval Kellner <yukellner@gmail.com>
2022-04-14 17:01:50 +0300

    /../

--- main-js/meme-controller.js
+++ main-js/meme-controller.js
@@ -52,10 +52,25 @@ function renderMeme() {
 
 function onDownload(){
     
-        var link = document.createElement('a');
-        link.download = 'filename.png';
-        link.href = document.getElementById('#my-canvas').toDataURL()
-        link.click();
+    
+
+        let canvasImage = document.getElementById('my-canvas').toDataURL('image/png');
+        
+        // this can be used to download any image from webpage to local disk
+        let xhr = new XMLHttpRequest();
+        xhr.responseType = 'blob';
+        xhr.onload = function () {
+            let a = document.createElement('a');
+            a.href = window.URL.createObjectURL(xhr.response);
+            a.download = 'image_name.png';
+            a.style.display = 'none';
+            document.body.appendChild(a);
+            a.click();
+            a.remove();
+          };
+          xhr.open('GET', canvasImage); // This is to download the canvas Image
+          xhr.send();
+    
       
 }
 
